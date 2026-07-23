f47e911bc10017ab5b4ad24ca522a94d0f641e78
realenv <real.env@outlook.com>
2026-07-24 00:46:31 +0100

    Adding Martial Arts 's React component to the skills page

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -1,11 +1,13 @@
 import ComputerScience from "../components/computerscience";
 import HumanLanguagesBadges from "../components/humanlanguagesbadges";
+import MartialArts from "../components/martial-arts";
 
 export default function Empty (){
     return (
         <>
             <ComputerScience/>
             <HumanLanguagesBadges/>
+            <MartialArts/>
         </>
     );
 }
